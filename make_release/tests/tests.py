4986b4e5fbfb5c754b37c71ce5d5cdc0a753b663
Wyatt Baldwin <self@wyattbaldwin.com>
2022-12-12 21:37:23 -0800

    Remove unit test that doesn't do anything

--- make_release/tests/tests.py
+++ make_release/tests/tests.py
@@ -1,5 +1,4 @@
 import doctest
-import unittest
 
 import make_release
 
@@ -7,8 +6,3 @@ import make_release
 def load_tests(loader, tests, ignore):
     tests.addTests(doctest.DocTestSuite(make_release))
     return tests
-
-
-class Tests(unittest.TestCase):
-    def test(self):
-        self.assertTrue(False)
